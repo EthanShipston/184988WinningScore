03fdc12dfbac10933de8ec9f0d5e2fd7e1207538
EthanShipston <36234265+EthanShipston@users.noreply.github.com>
2019-03-29 13:07:04 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _184988WinningScore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            int[] scores = new int[6];
+            string temp = txtInputs.Text;
+            for (int i = 0; i < 5; i++)
+            {
+                int.TryParse(temp.Substring(0, txtInputs.Text.IndexOf("\n")), out scores[i]);
+                temp = temp.Remove(0, temp.IndexOf("\n") + 1);
+            }
+            int.TryParse(temp, out scores[5]);
+
+            int A = (scores[0] * 3) + (scores[1] * 2) + scores[2];
+            int B = (scores[3] * 3) + (scores[4] * 2) + scores[5];
+
+            if (A < B)
+            {
+                lblOutput.Content = "B";
+            }
+            else if (A == B)
+            {
+                lblOutput.Content = "T";
+            }
+            else if (A > B)
+            {
+                lblOutput.Content = "A";
+            }
+        }
+    }
+}
